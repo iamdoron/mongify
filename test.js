1cdc5ece396a564836af1b7cfd39ed5e6cbc1217
doron pagot <doronpagot@gmail.com>
2013-09-14 02:15:22 +0300

    mongify a complex object

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ Chai.should();
 var it = Lab.test;
 
 Lab.experiment('mongify', function(){
-	it('should convert an ObjectID string to the Mongodb.ObjectID', function(done){
+	it('should convert an ObjectID string (24hex string) to a Mongodb.ObjectID', function(done){
 		Mongify('523396be6a51026f63000001').should.eql(new Mongodb.ObjectID('523396be6a51026f63000001'));
 		done();
 	}),
@@ -22,14 +22,14 @@ Lab.experiment('mongify', function(){
 		done();
 	}),
 
-	it('should convert an object with an ISO date string to an object with Date', function(done){
+	it('should convert an object with a 24-hex string to an ObjectID', function(done){
 		var anObject = {
 			content: { },
-			createdAt: '523396be6a51026f63000001'
+			_id: '523396be6a51026f63000001'
 		}
 		Mongify(anObject).should.eql({
 			content: { },
-			createdAt: new Mongodb.ObjectID('523396be6a51026f63000001')
+			_id: new Mongodb.ObjectID('523396be6a51026f63000001')
 		});
 
 		done();
@@ -38,9 +38,26 @@ Lab.experiment('mongify', function(){
 	it('should return a different object then the one it was converting', function(done){
 		var anObject = {
 			content: { },
-			createdAt: '523396be6a51026f63000009'
+			_id: '523396be6a51026f63000009'
 		}
 		Mongify(anObject).should.not.equal(anObject);
+
+		done();
+	}),
+
+	it('should convert an object with nested 24-hex strings to an object with ObjectIDs', function(done){
+		var anObject = {
+			number: 3441,
+			_id: new Mongodb.ObjectID(),
+			content: { content_id: '523396be6a51026f63000009', i: {am: {losing: {it_id: '523396be6a51026f63000009', yep: "i am"}}}},
+			super_id: '523396be6a51026f63000009'
+		}
+		Mongify(anObject).should.eql({
+			number: 3441,
+			_id: anObject._id,
+			content: { content_id: new Mongodb.ObjectID('523396be6a51026f63000009'), i: {am: {losing: {it_id: new Mongodb.ObjectID('523396be6a51026f63000009'), yep: "i am"}}}},
+			super_id: new Mongodb.ObjectID('523396be6a51026f63000009')
+		});
 
 		done();
 	});
